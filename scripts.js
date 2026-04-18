ff95502b78e75d509c89f62fd3201ca75c90889a
Jakob Rietzler <jakob.rietzler@gmail.com>
2026-04-18 19:07:13 +0200

    new win message

--- scripts.js
+++ scripts.js
@@ -133,7 +133,7 @@ function checkAnswer(userAnswer){
 
 function goToNextQuestion(){
     if (question_progress >= questions.order.length - 1) {
-        alert('Congratulations! You have completed all the questions!');
+        alert('Gewonnen - Alles gute zum geburtstag! zum 18. würde ich dich gerne zu einem Essen einladen - einem besonderem - ähnlich wie deine lieblings mario odessey challange :)');
         return;
     }
     question_progress++;
